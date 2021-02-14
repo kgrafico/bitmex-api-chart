78fbae0030f3d6722d791b811bbe51b63c8bf35d
kgrafico <carolinachasa@gmail.com>
2021-02-14 18:55:04 +0000

    feat: add axios call

--- Chart.tsx
+++ Chart.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export default ({ toggle }) => {
+const Chart = ({ toggle }) => {
+  const url =
+    "www.bitmex.com/realtime?subscribe=instrument,orderBookL2_25:XBTUSD";
+
+  const getallInformation = async () => {
+    try {
+      const userPosts = await axios.get(url);
+      console.log(userPosts.data);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getallInformation();
+    const interval = setInterval(() => {
+      getallInformation();
+    }, 60000);
+
+    return () => clearInterval(interval);
+  });
+
   if (toggle) {
-    return <h1>Hello!</h1>;
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
   }
+
   return "";
 };
+export default Chart;
