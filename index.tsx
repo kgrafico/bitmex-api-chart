bff70cfbebd0a8f7652d0a4aea404c315054f35d
kgrafico <carolinachasa@gmail.com>
2021-02-15 13:16:49 +0000

    2 files modified

--- index.tsx
+++ index.tsx
@@ -1,15 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 import Chart from "./Chart";
-import {
-  port,
-  testnet,
-  symbols,
-  streams,
-  apiKeyID,
-  apiKeySecret,
-  maxTableLen
-} from "./lib/config";
 
 const rootElement = document.getElementById("root");
 
@@ -17,16 +8,6 @@ import "./style.css";
 
 const App = () => {
   const [toggled, setToggled] = useState(false);
-
-  const config = {
-    port,
-    testnet,
-    symbols,
-    streams,
-    apiKeyID,
-    apiKeySecret,
-    maxTableLen
-  };
 
   const setToggle = () => {
     setToggled(!toggled);
